044e8948d72d1a0fef3b3d823674a56e756bc059
Samudra Perera <spperera@uwaterloo.ca>
2024-02-27 21:55:08 -0500

    Feat: sends token as cookie on login and clears cookie on logout

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -39,9 +39,6 @@ const register = async (req, res) => {
 const login = async (req, res) => {
   const { email, password } = req.body;
 
-  console.log("Cookies in request: \n");
-  console.log(req.cookies);
-
   try {
     const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
       email,
@@ -64,15 +61,12 @@ const login = async (req, res) => {
     res.cookie(String(user.rows[0].user_id), jwtToken, {
       domain: 'localhost',
       path: '/',
-      expires: new Date(Date.now() + 1000 * 86400 * 7),
+      expires: new Date(Date.now() + 1000 * 60 * 60), // Set expiration to 1 hour
       httpOnly: true,
       sameSite: 'lax',
       secure: false,
-    })
-
-    console.log("Cookies set in response:");
-    console.log(res.get('Set-Cookie'));
-
+    });
+    
     return res.status(200).json({ message: 'Succefully Logged In', token: jwtToken, user });
   } catch (err) {
     return res.status(500).send("Server Error");
@@ -101,12 +95,13 @@ const deleteUser = async (req, res) => {
   }
 }
 
-const logout = async (req, res, next) => {
+const logout = async (req, res) => {
   const cookies = req.headers.cookie
   const token = cookies ? cookies.split('=')[1] : null;
 
   console.log("Cookies in request: \n");
   console.log(req.cookies);
+  
 
   if (!token) {
     // handle this scenario accordingly
@@ -118,9 +113,8 @@ const logout = async (req, res, next) => {
       // handle scenario, flag suspicious activity?
       return res.status(400).json({ message: "Authentication Failed" })
     }
-
     // expected logout conditions; clear cookies and return
-    res.clearCookie(`${user.user_id}`, {
+    res.clearCookie(`${user.user.id}`, {
       domain: 'localhost',
       path: '/',
       // Set the expires option to a past date to invalidate the cookie
@@ -129,19 +123,30 @@ const logout = async (req, res, next) => {
       sameSite: 'lax',
       secure: false,
     });
-    req.cookies[`${user.user_id}`] = ''; // apparently this is read only, so this line might not be doing anything
-
-    console.log("Cookies in response: \n");
-    console.log(res.cookies);
-
-    console.log("Cookies set in response:");
-    console.log(res.get('Set-Cookie'));
+    req.cookies[`${user.user.id}`] = ''; // apparently this is read only, so this line might not be doing anything
 
     return res.status(200).json({ message: "Successfully Logged out" })
   })
+}
+
+const verifyToken = async (req, res) => {
+  const cookies = req.headers.cookie
+  const token = cookies.split('=')[1]
+
+  if(!token) {
+    res.status(404).json({message: 'No Token Found'})
+  }
+  jwt.verify(String(token), process.env.JWT_SECRET_KEY, (err, user) => {
+    if(err) {
+      return res.status(400).json({message: "Invalid Token"})
+    }
+    return res.status(200).json({auth: true})
+  })
+
 }
 
 exports.register = register;
 exports.login = login;
 exports.deleteUser = deleteUser
 exports.logout = logout
+exports.verifyToken = verifyToken
