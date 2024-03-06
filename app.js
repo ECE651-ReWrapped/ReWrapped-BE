ae2a2ba0a0eda1fd2a56b1de31b45f62fa900948
Imran Ahmed <i33ahmed@uwaterloo.ca>
2024-03-06 15:54:48 -0500

    Imran: Added spotify dashboard in MVC structure

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require("dotenv").config(); // should be the first thing that runs
 
 const express = require("express");
+const session = require("express-session");
+const sessionSecret = process.env.SESSION_SECRET || "your-secret-key";
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/userRoutes");
+const authRoutes = require('./routes/apiRoutes');
 
 const app = express();
 
@@ -11,7 +14,12 @@ const app = express();
 app.use(cors({ credentials: true, origin: process.env.REACT_APP_CLIENT }));
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+    session({ secret: sessionSecret, resave: true, saveUninitialized: true })
+  );
 
 app.use("/", userRoutes);
+app.use("/", authRoutes);
 
 module.exports = app;
