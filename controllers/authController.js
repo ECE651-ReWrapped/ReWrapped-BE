d3200e4f1bd4fdb6e8858dfc278c1997f57a2a53
Daniel Almeida <d2almeid@uwaterloo.ca>
2024-04-04 03:55:06 -0400

    Loading listening trends needs debugging; need to stop it from loading streams on each login

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,7 +8,7 @@ const { Pool } = require('pg');
 const pool = require('../db'); // Reuse the existing pool
 const moment = require('moment-timezone');
 const { generateRandomString, shuffleArray } = require('../utils/spotifyUtils');
-const { getRecommendedTracks, getRecentlyPlayedTracks , getTopGenres , getListeningTrends } = require('./trackServices');
+const { getRecommendedTracks, getRecentlyPlayedTracks, getTopGenres, getListeningTrends } = require('./trackServices');
 
 // Other imports (like getUserById) should be added based on your application structure
 
@@ -86,7 +86,7 @@ const authController = {
                 json: true,
               };
 
-              request.get(recentlyPlayedOptions, function (error, recentlyPlayedResponse, recentlyPlayedBody) {
+              request.get(recentlyPlayedOptions, async function (error, recentlyPlayedResponse, recentlyPlayedBody) {
                 if (!error && recentlyPlayedResponse.statusCode === 200) {
 
                   // Extract relevant information from recently played tracks
@@ -144,19 +144,31 @@ const authController = {
                     // Update listening trends
                     const trackDate = moment.utc(item.played_at).format('YYYY-MM-DD'); // Ensures the date is interpreted in UTC
 
-                    const updateListeningTrendsQuery = `
-                    INSERT INTO listening_trends (user_name, date, track_count)
-                    VALUES ($1, $2, 1)
-                    ON CONFLICT (user_name, date)
-                    DO UPDATE SET track_count = listening_trends.track_count + 1`;
+                    const checkQuery = `
+        SELECT 1
+        FROM recently_played_tracks
+        WHERE user_name = $1 AND track_name = $2`;
+                    const checkValues = [userId, item.track.name];
 
-                    const trendValues = [userId, trackDate];
+                    const { rows } = pool.query(checkQuery, checkValues);
 
-                    pool.query(updateListeningTrendsQuery, trendValues, (err) => {
-                      if (err) {
-                        console.error('Error updating listening trends:', err);
-                      }
-                    });
+                    console.log(rows);
+
+                    if (!rows) {
+                      // Track has not been counted yet, update listening trends
+                      const updateListeningTrendsQuery = `
+          INSERT INTO listening_trends (user_name, date, track_count)
+          VALUES ($1, $2, 1)
+          ON CONFLICT (user_name, date)
+          DO UPDATE SET track_count = listening_trends.track_count + 1;`;
+                      const trendValues = [userId, trackDate];
+
+                      pool.query(updateListeningTrendsQuery, trendValues, (err) => {
+                        if (err) {
+                          console.error('Error updating listening trends:', err);
+                        }
+                      });
+                    }
 
                     // Store the track information along with genres
                     recentlyPlayedTracks.push({
@@ -301,8 +313,8 @@ const authController = {
     }
   },
 
-   // API endpoint to get listening trends
-   getListeningTrend: async (req, res) => {
+  // API endpoint to get listening trends
+  getListeningTrend: async (req, res) => {
     try {
       const userId = req.params.userId; // User ID parameter from the request
 
